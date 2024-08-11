dddef9b2ab393cb29e57690ca99f9986e3d88dec
m1xa4kala <maikl2020011@gmail.com>
2024-08-11 23:40:31 +0300

    setup pre-commit with husky

--- frontend/src/pages/home/ui/HomePage.tsx
+++ frontend/src/pages/home/ui/HomePage.tsx
@@ -1,85 +1,85 @@
 import { TicketCard } from '@/entities/ticket'
 
 export const HomePage = () => {
-	return (
-		<div>
-			<TicketCard
-				airline='Аэрофлот'
-				price={30000}
-				departureTime='10:00'
-				arrivalTime='12:00'
-				origin='Москва'
-				destination='Иваново'
-				duration='2ч 30мин'
-				transfersCount={1}
-				transferDuration='1ч'
-			/>
-			<TicketCard
-				airline='Аэрофлот'
-				price={30000}
-				departureTime='10:00'
-				arrivalTime='12:00'
-				origin='Москва'
-				destination='Иваново'
-				duration='2ч 30мин'
-				transfersCount={1}
-				transferDuration='1ч'
-			/>
-			<TicketCard
-				airline='Аэрофлот'
-				price={30000}
-				departureTime='10:00'
-				arrivalTime='12:00'
-				origin='Москва'
-				destination='Иваново'
-				duration='2ч 30мин'
-				transfersCount={1}
-				transferDuration='1ч'
-			/>
-			<TicketCard
-				airline='Аэрофлот'
-				price={30000}
-				departureTime='10:00'
-				arrivalTime='12:00'
-				origin='Москва'
-				destination='Иваново'
-				duration='2ч 30мин'
-				transfersCount={1}
-				transferDuration='1ч'
-			/>
-			<TicketCard
-				airline='Аэрофлот'
-				price={30000}
-				departureTime='10:00'
-				arrivalTime='12:00'
-				origin='Москва'
-				destination='Иваново'
-				duration='2ч 30мин'
-				transfersCount={1}
-				transferDuration='1ч'
-			/>
-			<TicketCard
-				airline='Аэрофлот'
-				price={30000}
-				departureTime='10:00'
-				arrivalTime='12:00'
-				origin='Москва'
-				destination='Иваново'
-				duration='2ч 30мин'
-				transfersCount={1}
-				transferDuration='1ч'
-			/>
-			<TicketCard
-				airline='Аэрофлот'
-				price={30000}
-				departureTime='10:00'
-				arrivalTime='12:00'
-				origin='Москва'
-				destination='Иваново'
-				duration='2ч 30мин'
-				transfersCount={1}
-				transferDuration='1ч'
-			/>
-		</div>
-	)
+  return (
+    <div>
+      <TicketCard
+        airline='Аэрофлот'
+        price={30000}
+        departureTime='10:00'
+        arrivalTime='12:00'
+        origin='Москва'
+        destination='Иваново'
+        duration='2ч 30ми'
+        transfersCount={1}
+        transferDuration='1ч'
+      />
+      <TicketCard
+        airline='Аэрофлот'
+        price={30000}
+        departureTime='10:00'
+        arrivalTime='12:00'
+        origin='Москва'
+        destination='Иваново'
+        duration='2ч 30мин'
+        transfersCount={1}
+        transferDuration='1ч'
+      />
+      <TicketCard
+        airline='Аэрофлот'
+        price={30000}
+        departureTime='10:00'
+        arrivalTime='12:00'
+        origin='Москва'
+        destination='Иваново'
+        duration='2ч 30мин'
+        transfersCount={1}
+        transferDuration='1ч'
+      />
+      <TicketCard
+        airline='Аэрофлот'
+        price={30000}
+        departureTime='10:00'
+        arrivalTime='12:00'
+        origin='Москва'
+        destination='Иваново'
+        duration='2ч 30мин'
+        transfersCount={1}
+        transferDuration='1ч'
+      />
+      <TicketCard
+        airline='Аэрофлот'
+        price={30000}
+        departureTime='10:00'
+        arrivalTime='12:00'
+        origin='Москва'
+        destination='Иваново'
+        duration='2ч 30мин'
+        transfersCount={1}
+        transferDuration='1ч'
+      />
+      <TicketCard
+        airline='Аэрофлот'
+        price={30000}
+        departureTime='10:00'
+        arrivalTime='12:00'
+        origin='Москва'
+        destination='Иваново'
+        duration='2ч 30мин'
+        transfersCount={1}
+        transferDuration='1ч'
+      />
+      <TicketCard
+        airline='Аэрофлот'
+        price={30000}
+        departureTime='10:00'
+        arrivalTime='12:00'
+        origin='Москва'
+        destination='Иваново'
+        duration='2ч 30мин'
+        transfersCount={1}
+        transferDuration='1ч'
+      />
+    </div>
+  )
 }
