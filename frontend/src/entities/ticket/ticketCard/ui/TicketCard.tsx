2e49b9548967f2e370587e26e63b63a33bdf00ba
m1xa4kala <maikl2020011@gmail.com>
2024-09-18 13:05:11 +0300

    Format dates and fix some styles

--- frontend/src/entities/ticket/ticketCard/ui/TicketCard.tsx
+++ frontend/src/entities/ticket/ticketCard/ui/TicketCard.tsx
@@ -8,16 +8,27 @@ type TicketCardProps = {
 }
 
 export const TicketCard: React.FC<TicketCardProps> = ({ ticket }) => {
+  const departureTime = new Date(ticket.departureTime).toLocaleTimeString(
+    'ru-RU',
+    {
+      hour: '2-digit',
+      minute: '2-digit',
+    }
+  )
+  const arrivalTime = new Date(ticket.arrivalTime).toLocaleTimeString('ru-RU', {
+    hour: '2-digit',
+    minute: '2-digit',
+  })
   return (
     <div className='ticket__card'>
       <div className='ticket__card-header'>
         <div className='ticket__card-departure'>
           <h2 className='departure__point'>{ticket.origin}</h2>
-          <h3 className='departure__time'>{ticket.departureTime}</h3>
+          <h3 className='departure__time'>{departureTime}</h3>
         </div>
         <div className='ticket__card-destination'>
           <h2 className='destination__point'>{ticket.destination}</h2>
-          <h3 className='destination__time'>{ticket.arrivalTime}</h3>
+          <h3 className='destination__time'>{arrivalTime}</h3>
         </div>
       </div>
       <div className='ticket__card-info'>
