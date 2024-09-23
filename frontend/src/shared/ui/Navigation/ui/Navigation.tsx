0116cdf31238fc77b59f0282295deb5a96a70a16
m1xa4kala <maikl2020011@gmail.com>
2024-09-23 09:48:36 +0300

    Added styles to navigation

--- frontend/src/shared/ui/Navigation/ui/Navigation.tsx
+++ frontend/src/shared/ui/Navigation/ui/Navigation.tsx
@@ -1,19 +1,21 @@
 import { NavLink } from 'react-router-dom'
 
+import './Navigation.scss'
+
 export const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className='navigation'>
+      <ul className='navigation__list'>
+        <li className='navigation__item'>
           <NavLink to='/'>Главная</NavLink>
         </li>
-        <li>
+        <li className='navigation__item'>
           <NavLink to='/admin'>Админ</NavLink>
         </li>
-        <li>
+        <li className='navigation__item'>
           <NavLink to='/profile'>Профиль</NavLink>
         </li>
-        <li>
+        <li className='navigation__item'>
           <NavLink to='/favorits'>Избранное</NavLink>
         </li>
       </ul>
