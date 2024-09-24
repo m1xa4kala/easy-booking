d8695b6014f1bf3a9b45a89ab021b486d021cb53
m1xa4kala <maikl2020011@gmail.com>
2024-09-24 22:28:43 +0300

    Centered Search layout content

--- frontend/src/app/layouts/SearchLayout/ui/SearchLayout.tsx
+++ frontend/src/app/layouts/SearchLayout/ui/SearchLayout.tsx
@@ -2,11 +2,14 @@ import { Outlet } from 'react-router-dom'
 import { SearchForm } from '@/features/searchForm'
 import { Container } from '@/shared/ui'
 
+import './SearchLayout.scss'
 export const SearchLayout = () => {
   return (
     <Container>
-      <SearchForm />
-      <Outlet />
+      <div className='search-layout'>
+        <SearchForm />
+        <Outlet />
+      </div>
     </Container>
   )
 }
