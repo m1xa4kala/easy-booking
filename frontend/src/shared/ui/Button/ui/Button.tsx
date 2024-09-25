61662ee3411bf2c1c7d5eff98cc4065c57afc625
m1xa4kala <maikl2020011@gmail.com>
2024-09-25 20:57:32 +0300

    Added outline style for Button

--- frontend/src/shared/ui/Button/ui/Button.tsx
+++ frontend/src/shared/ui/Button/ui/Button.tsx
@@ -3,11 +3,15 @@ import './Button.scss'
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
+  variant?: 'outline'
 }
 
 export const Button: React.FC<IButtonProps> = ({ children, ...props }) => {
+  const variant =
+    props.variant === 'outline' ? 'button__outline' : 'button__primary'
+  const className = `${variant} ${props.className}`
   return (
-    <button {...props} className={`button ${props.className}`}>
+    <button {...props} className={className}>
       {children}
     </button>
   )
